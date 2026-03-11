6ef5eb10211163280a0bb783cb4886f183b10939
IsroilovA <108577814+IsroilovA@users.noreply.github.com>
2026-03-12 01:55:01 +0500

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:appfuse/appfuse.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pauza/firebase_options.dart';
 import 'package:pauza/src/app/pauza_app.dart';
 import 'package:pauza/src/core/init/pauza_dependencies.dart';
 import 'package:pauza/src/core/common_ui/pauza_splash_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     AppFuseScope(
       themes: PauzaApp.themes,

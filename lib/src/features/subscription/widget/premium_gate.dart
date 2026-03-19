95afd44c21f9aac68fa8931d031b12d07ec93fb1
IsroilovA <108577814+IsroilovA@users.noreply.github.com>
2026-03-20 00:57:46 +0500

    sync changes

--- lib/src/features/subscription/widget/premium_gate.dart
+++ lib/src/features/subscription/widget/premium_gate.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pauza/src/app/root_scope.dart';
 import 'package:pauza/src/features/profile/common/bloc/current_user_bloc.dart';
 import 'package:pauza/src/features/profile/common/bloc/current_user_state.dart';
 
@@ -13,7 +12,6 @@ class PremiumGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocSelector<CurrentUserBloc, CurrentUserState, bool>(
-      bloc: RootScope.of(context).currentUserBloc,
       selector: (state) => state.user?.subscription?.isActive == true,
       builder: (context, isPremium) => isPremium ? child : lockedChild,
     );
